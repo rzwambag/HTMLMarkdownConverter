743574386119e637c92b5062e2439db7d19c8459
rzwambag <mail@remcozwambag.nl>
2024-04-30 15:45:00 +0200

    Add documentation

--- HTML-Markdown-Converter/IHTMLMarkdownConverter.cs
+++ HTML-Markdown-Converter/IHTMLMarkdownConverter.cs
@@ -2,10 +2,15 @@
 
 namespace HTMLMarkdown
 {
-    [OSInterface]
+    [OSInterface(Description = "Facilitates conversion from HTML to Markdown and vice versa.", Name = "HTML-Markdown-Converter")]
     public interface IHTMLMarkdownConverter
     {
-        public string HTMLToMarkdown(string html);
-        public string MarkdownToHTML(string markdown);
+        [OSAction(Description = "Converts HTML to Markdown.", ReturnName = "Markdown", ReturnType = OSDataType.Text)]
+        public string HTMLToMarkdown(
+            [OSParameter(DataType = OSDataType.Text, Description = "The HTML which needs to be converted to HTML")] string html);
+
+        [OSAction(Description = "Converts Markdown to HTMl.", ReturnName = "HTML", ReturnType = OSDataType.Text)]
+        public string MarkdownToHTML(
+            [OSParameter(DataType = OSDataType.Text, Description = "The Markdown which needs to be converted to HTML")] string markdown);
     }
 }
--- README.md
+++
@@ -1,1 +0,0 @@
-# HTML-Markdown-Converter
